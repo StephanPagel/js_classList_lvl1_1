bb0a0d6e2da93b1539621686a7f198b0400152c4
StephanPagel <stephan.pagel84@gmail.com>
2022-04-20 11:05:01 +0200

    finished

---
+++ assets/js/main.js
@@ -0,0 +1,18 @@
+let headline = document.getElementById('headline');
+let bucketlist = document.getElementById('bucketlist');
+
+headline.classList.add('headline');
+bucketlist.classList.add('bucketlist');
+
+function styleHeadline() {
+    headline.classList.add('headline');
+}
+
+function unstyleHeadline() {
+    headline.classList.remove('headline');
+}
+
+
+
+
+
